2bb70f2dc00c06a82a2d2b982c6fc4980e022946
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-08-21 13:16:34 +0530

    "model done"

--- models/taskModel.js
+++ models/taskModel.js
@@ -217,6 +217,249 @@ const taskSchema = new Schema({
   tdsmonthly_paidDate: {
     type: Date,
   },
+  //income TAX- Model here
+
+  tax_taskName: {
+    type: String,
+    enum: [
+      "incomeTaxAuditCases",
+      "incomeTaxNonAuditCases",
+      "incomeTaxAdvanceTax",
+    ],
+    required: true,
+  },
+  tax_formStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+  tax_filingStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+  tax_paymentStatus: {
+    type: String,
+    enum: ["taxPaid", "taxNotPaid", "partiallyPaid", "verified", "notVerified"],
+  },
+  tax_paymentAmount: {
+    type: Number,
+  },
+  tax_paymentDate: {
+    type: Date,
+  },
+  tax_verifiedDate: {
+    type: Date,
+  },
+  tax_quarter: {
+    type: String,
+    enum: ["quarter1", "quarter2", "quarter3", "quarter4"],
+  },
+
+  // Additional fields for different scenarios
+  tax_audit_formStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+  tax_audit_filingStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+  tax_audit_paymentStatus: {
+    type: String,
+    enum: ["taxPaid", "taxNotPaid", "partiallyPaid", "verified"],
+  },
+  tax_audit_paymentAmount: {
+    type: Number,
+  },
+  tax_audit_paymentDate: {
+    type: Date,
+  },
+
+  tax_nonAudit_filingStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+  tax_nonAudit_paymentStatus: {
+    type: String,
+    enum: ["taxPaid", "taxNotPaid", "partiallyPaid", "verified", "notVerified"],
+  },
+  tax_nonAudit_paymentAmount: {
+    type: Number,
+  },
+  tax_nonAudit_paymentDate: {
+    type: Date,
+  },
+  tax_nonAudit_verifiedDate: {
+    type: Date,
+  },
+
+  tax_advanceTax_quarter: {
+    type: String,
+    enum: ["quarter1", "quarter2", "quarter3", "quarter4"],
+  },
+  tax_advanceTax_paymentStatus: {
+    type: String,
+    enum: ["taxPaid", "taxNotPaid", "partiallyPaid", "verified"],
+  },
+  tax_advanceTax_paymentAmount: {
+    type: Number,
+  },
+  tax_advanceTax_paymentDate: {
+    type: Date,
+  },
+  tax_advanceTax_filingStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+
+  //esi - model
+
+  taskName: {
+    type: String,
+    enum: ["esiNewRegistration", "esiRegularMonthlyActivity", "esiInactive"],
+    required: true,
+  },
+  esi_new_applicationStatus: {
+    type: String,
+    enum: ["pendingForApply", "applied"],
+  },
+  esi_new_applicationNumber: {
+    type: String,
+  },
+  esi_new_applicationDate: {
+    type: Date,
+  },
+  esi_new_applicationSubStatus: {
+    type: String,
+    enum: [
+      "pendingForApproval",
+      "pendingForClarification",
+      "rejected",
+      "approved",
+    ],
+  },
+  esi_new_dateOfApproval: {
+    type: Date,
+  },
+
+  esi_typeOfGSTForm: {
+    type: String,
+    enum: ["gstr1", "gstr3b"],
+  },
+  esi_filingStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+  esi_previousMonthNotFiled: {
+    type: String,
+    enum: ["yes", "no"],
+  },
+  esi_currentStatus: {
+    type: String,
+    enum: ["documentsPending", "waitingForClarification", "workInProgress"],
+  },
+  esi_fileDate: {
+    type: Date,
+  },
+
+  esi_inactive_applicationStatus: {
+    type: String,
+    enum: ["closureApplied", "inactiveDueToNonFiling"],
+  },
+  esi_inactive_applicationNumber: {
+    type: String,
+  },
+  esi_inactive_applicationDate: {
+    type: Date,
+  },
+  esi_inactive_applicationSubStatus: {
+    type: String,
+    enum: [
+      "pendingForApproval",
+      "pendingForClarification",
+      "rejected",
+      "approved",
+    ],
+  },
+  esi_inactive_dateOfApproval: {
+    type: Date,
+  },
+  //professional tax
+
+  // Task Name
+  pft_taskName: {
+    type: String,
+    enum: [
+      "professionalTaxNewRegistration",
+      "professionalTaxRegularMonthlyActivity",
+      "professionalTaxInactive",
+    ],
+    required: true,
+  },
+
+  // Professional Tax - New Registration
+  pft_applicationStatus: {
+    type: String,
+    enum: ["pendingForApply", "applied"],
+  },
+  pft_applicationNumber: {
+    type: String,
+  },
+  pft_applicationDate: {
+    type: Date,
+  },
+  pft_applicationSubStatus: {
+    type: String,
+    enum: [
+      "pendingForApproval",
+      "pendingForClarification",
+      "rejected",
+      "approved",
+    ],
+  },
+  pft_new_dateOfApproval: {
+    type: Date,
+  },
+
+  // Professional Tax - Regular Monthly Activity
+  pft_typeOfGSTForm: {
+    type: String,
+    enum: ["gstr1"],
+  },
+  pft_filingStatus: {
+    type: String,
+    enum: ["filed", "notFiled"],
+  },
+  pft_currentStatus: {
+    type: String,
+    enum: ["documentsPending", "waitingForClarification", "workInProgress"],
+  },
+  pft_fileDate: {
+    type: Date,
+  },
+
+  // Professional Tax - Inactive
+  pft_inactive_applicationStatus: {
+    type: String,
+    enum: ["closureApplied", "inactiveDueToNonFiling"],
+  },
+  pft_inactive_applicationNumber: {
+    type: String,
+  },
+  pft_inactive_applicationDate: {
+    type: Date,
+  },
+  pft_inactive_applicationSubStatus: {
+    type: String,
+    enum: [
+      "pendingForApproval",
+      "pendingForClarification",
+      "rejected",
+      "approved",
+    ],
+  },
+  pft_inactive_dateOfApproval: {
+    type: Date,
+  },
 });
 
 const taskModel = mongoose.model("Task", taskSchema);
