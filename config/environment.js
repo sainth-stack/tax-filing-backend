90a8d52b7dff5fab36aad09db345366f5bf9854d
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-09-23 23:50:41 +0530

    'done'

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ export const PORT = 4500;
 
 export const DATABASE_URL = `mongodb+srv://prashanth:BnHRQrqZHdnosfEe@cluster0.cpydc.mongodb.net/tax-filing?retryWrites=true&w=majority`;
 
-//const DATABASE_URL = "mongodb://localhost:27017/talentSpotifyDB"
+//const DATABASE_URL = "mongodb://localhost:27017/"
 
 export const JWT_SECRET =
   "$2a$12$e9HMflla.nPm9t8LOopEdeTYD.fmAYbxQ4p6XbLNylKRgcDEMXPba";
