4af06697088361732f2a007ad8450d7415511994
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-12-14 16:54:08 +0530

    "done"

--- models/companyModel.js
+++ models/companyModel.js
@@ -7,58 +7,70 @@ const companySchema = new mongoose.Schema(
         type: String,
         required: [true, "Company name is required"],
       },
-      
+
       constitution: {
         type: String,
-        required: [true, "Constitution  is required"],
+        // required: [true, "Constitution  is required"],
       },
       subConstitution: {
         type: String,
-        required: [true, "SubConstitution  is required"],
+        // required: [true, "SubConstitution  is required"],
       },
       clientStatus: {
         type: String,
-        required: [true, "ClientStatus  is required"],
+        // required: [true, "ClientStatus  is required"],
       },
       authorisedPerson: {
         type: String,
-        required: [true, "AuthorisedPerson  is required"],
+        // required: [true, "AuthorisedPerson  is required"],
       },
-      phone: { type: String, required: [true, "Phone  is required"] },
-      mailId: { type: String, required: [true, "MailId  is required"] },
-      pan: { type: String, required: [true, "Pan  is required"] },
+      phone: {
+        type: String,
+        // required: [true, "Phone  is required"],
+      },
+      mailId: {
+        type: String,
+        required: [true, "MailId  is required"],
+      },
+      pan: {
+        type: String,
+        required: [true, "Pan  is required"],
+      },
       companyAddress: { type: String },
       effectiveFrom: {
         type: String,
-        required: [true, "EffectiveFrom  is required"],
+        // required: [true, "EffectiveFrom  is required"],
       },
       effectiveTo: {
         type: String,
-        required: [true, "EffectiveTo  is required"],
+        // required: [true, "EffectiveTo  is required"],
       },
     },
 
     incomeTax: {
       incomeTaxPassword: {
         type: String,
-        required: [true, "Income TaxPassword is required"],
+        // required: [true, "Income TaxPassword is required"],
       },
       incomeTraceTaxUsername: {
         type: String,
-        required: [true, "Income TraceTax Username is required"],
+        // required: [true, "Income TraceTax Username is required"],
       },
       incomeTraceTaxPassword: {
         type: String,
-        required: [true, "Income TraceTax Password is required"],
+        // required: [true, "Income TraceTax Password is required"],
       },
-      status: { type: String, required: [true, "Status is required"] },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
       effectiveFrom: {
         type: String,
-        required: [true, "EffectiveFrom is required"],
+        // required: [true, "EffectiveFrom is required"],
       },
       effectiveTo: {
         type: String,
-        required: [true, "EffectiveTo is required"],
+        // required: [true, "EffectiveTo is required"],
       },
       approvalCertificate: {
         type: String,
@@ -67,35 +79,41 @@ const companySchema = new mongoose.Schema(
     },
 
     gst: {
-      gstin: { type: String, required: [true, "GSTIN is required"] },
+      gstin: {
+        type: String,
+        // required: [true, "GSTIN is required"]
+      },
       gstUserName: {
         type: String,
-        required: [true, "GST UserName is required"],
+        // required: [true, "GST UserName is required"],
       },
       gstPassword: {
         type: String,
-        required: [true, "GST Password is required"],
+        // required: [true, "GST Password is required"],
       },
-      status: { type: String, required: [true, "Status is required"] },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
       eWayBillUsername: {
         type: String,
-        required: [true, "EWayBill Username is required"],
+        // required: [true, "EWayBill Username is required"],
       },
       eWayBillPassword: {
         type: String,
-        required: [true, "EWayBill Password is required"],
+        // required: [true, "EWayBill Password is required"],
       },
       effectiveFrom: {
         type: String,
-        required: [true, "EffectiveFrom is required"],
+        // required: [true, "EffectiveFrom is required"],
       },
       effectiveTo: {
         type: String,
-        required: [true, "EffectiveTo is required"],
+        // required: [true, "EffectiveTo is required"],
       },
       typeOfGstFiling: {
         type: String,
-        required: [true, "TypeOfGstFiling is required"],
+        // required: [true, "TypeOfGstFiling is required"],
       },
       dueDateReturn: { type: String },
       approvalCertificate: {
@@ -105,12 +123,28 @@ const companySchema = new mongoose.Schema(
     },
 
     esi: {
-      esiNumber: { type: String, required: [true, "Esi Number is required"] },
-      esiUserId: { type: String, required: [true, "Esi UserId is required"] },
-      esiPassword: { type: String, required: [true, "Esi Password is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      esiNumber: { type: String,
+        // required: [true, "Esi Number is required"]
+      },
+      esiUserId: { type: String,
+        // required: [true, "Esi UserId is required"]
+      },
+      esiPassword: {
+        type: String,
+        // required: [true, "Esi Password is required"],
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -118,12 +152,26 @@ const companySchema = new mongoose.Schema(
     },
 
     providentFund: {
-      pfNumber: { type: String, required: [true, "PF Number is required"] },
-      pfUserId: { type: String,required: [true, "PF UserId is required"] },
-      pfPassword: { type: String,required: [true, "PF Password is required"] },
-      status: { type: String,required: [true, "Status is required"] },
-      effectiveFrom: { type: String,required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String,required: [true, "EffectiveTo is required"] },
+      pfNumber: { type: String, 
+        // required: [true, "PF Number is required"]
+       },
+      pfUserId: { type: String, 
+        // required: [true, "PF UserId is required"]
+       },
+      pfPassword: { type: String,
+        //  required: [true, "PF Password is required"]
+         },
+      status: { type: String,
+        //  required: [true, "Status is required"]
+         },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -131,17 +179,44 @@ const companySchema = new mongoose.Schema(
     },
 
     professionalTax: {
-      ptEcNumber: { type: String, required: [true, "PT EC Number is required"] },
-      ptUsername: { type: String, required: [true, "PT Username is required"] },
+      ptEcNumber: {
+        type: String,
+        // required: [true, "PT EC Number is required"],
+      },
+      ptUsername: {
+        type: String,
+        // required: [true, "PT Username is required"]
+      },
 
-      ptPassword: { type: String, required: [true, "PT Password is required"] },
-      ptEcUsername: { type: String, required: [true, "PT EC Username is required"] },
-      ptEcPassword: { type: String, required: [true, "PT EC Password is required"] },
+      ptPassword: {
+        type: String,
+        // required: [true, "PT Password is required"]
+      },
+      ptEcUsername: {
+        type: String,
+        // required: [true, "PT EC Username is required"],
+      },
+      ptEcPassword: {
+        type: String,
+        // required: [true, "PT EC Password is required"],
+      },
 
-      ptNumber: { type: String, required: [true, "PT Number is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      ptNumber: {
+        type: String,
+        // required: [true, "PT Number is required"]
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -149,13 +224,34 @@ const companySchema = new mongoose.Schema(
     },
 
     tds: {
-      tan: { type: String, required: [true, "TAN is required"] },
-      tanPassword: { type: String, required: [true, "TAN Password is required"] },
-      tracesUsername: { type: String, required: [true, "Traces Username is required"] },
-      tracesPassword: { type: String, required: [true, "Traces Password is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      tan: {
+        type: String,
+        // required: [true, "TAN is required"]
+      },
+      tanPassword: {
+        type: String,
+        // required: [true, "TAN Password is required"],
+      },
+      tracesUsername: {
+        type: String,
+        // required: [true, "Traces Username is required"],
+      },
+      tracesPassword: {
+        type: String,
+        // required: [true, "Traces Password is required"],
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -163,13 +259,35 @@ const companySchema = new mongoose.Schema(
     },
 
     shopCommercialEstablishment: {
-      seNumber: { type: String, required: [true, "SE Number is required"] },
-      seUsername: { type: String, required: [true, "SE Username is required"] },
-      sePassword: { type: String, required: [true, "SE Password is required"] },
-      seRenewalDate: { type: String, required: [true, "SE Renewal Date is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      seNumber: {
+        type: String,
+        // required: [true, "SE Number is required"]
+      },
+      seUsername: {
+        type: String,
+        // required: [true, "SE Username is required"]
+      },
+      sePassword: {
+        type: String,
+        // required: [true, "SE Password is required"]
+      },
+      seRenewalDate: {
+        type: String,
+        // required: [true, "SE Renewal Date is required"],
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -177,10 +295,22 @@ const companySchema = new mongoose.Schema(
     },
 
     msme: {
-      msmeNumber: { type: String, required: [true, "MSME Number is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      msmeNumber: {
+        type: String,
+        // required: [true, "MSME Number is required"]
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -188,13 +318,34 @@ const companySchema = new mongoose.Schema(
     },
 
     fssai: {
-      fssaiNumber: { type: String, required: [true, "FSSAI Number is required"] },
-      fssaiUsername: { type: String, required: [true, "FSSAI Username is required"] },
-      fssaiPassword: { type: String, required: [true, "FSSAI Password is required"] },
-      fssaiRenewalDate: { type: String, required: [true, "FSSAI Renewal Date is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      fssaiNumber: {
+        type: String,
+        // required: [true, "FSSAI Number is required"],
+      },
+      fssaiUsername: {
+        type: String,
+        // required: [true, "FSSAI Username is required"],
+      },
+      fssaiPassword: {
+        type: String,
+        // required: [true, "FSSAI Password is required"],
+      },
+      fssaiRenewalDate: {
+        type: String,
+        // required: [true, "FSSAI Renewal Date is required"],
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -202,13 +353,31 @@ const companySchema = new mongoose.Schema(
     },
 
     factoryLicense: {
-      flNumber: { type: String, required: [true, "FL Number is required"] },
-      flUsername: { type: String, required: [true, "FL Username is required"] },
-      flPassword: { type: String, required: [true, "FL Password is required"] },
-      flRenewalDate: { type: String, required: [true, "FL Renewal Date is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      flNumber: { type: String, 
+        // required: [true, "FL Number is required"] 
+      },
+      flUsername: { type: String, 
+        // required: [true, "FL Username is required"] 
+      },
+      flPassword: { type: String,
+        //  required: [true, "FL Password is required"] 
+        },
+      flRenewalDate: {
+        type: String,
+        // required: [true, "FL Renewal Date is required"],
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
@@ -216,14 +385,38 @@ const companySchema = new mongoose.Schema(
     },
 
     importExport: {
-      iecNumber: { type: String, required: [true, "IEC Number is required"] },
-      dgftUsername: { type: String, required: [true, "DGFT Username is required"] },
-      dgftPassword: { type: String, required: [true, "DGFT Password is required"] },
-      icegateUsername: { type: String, required: [true, "ICEGATE Username is required"] },
-      icegatePassword: { type: String, required: [true, "ICEGATE Password is required"] },
-      status: { type: String, required: [true, "Status is required"] },
-      effectiveFrom: { type: String, required: [true, "EffectiveFrom is required"] },
-      effectiveTo: { type: String, required: [true, "EffectiveTo is required"] },
+      iecNumber: {
+        type: String,
+        // required: [true, "IEC Number is required"]
+      },
+      dgftUsername: {
+        type: String,
+        // required: [true, "DGFT Username is required"],
+      },
+      dgftPassword: {
+        type: String,
+        // required: [true, "DGFT Password is required"],
+      },
+      icegateUsername: {
+        type: String,
+        // required: [true, "ICEGATE Username is required"],
+      },
+      icegatePassword: {
+        type: String,
+        // required: [true, "ICEGATE Password is required"],
+      },
+      status: {
+        type: String,
+        // required: [true, "Status is required"]
+      },
+      effectiveFrom: {
+        type: String,
+        // required: [true, "EffectiveFrom is required"],
+      },
+      effectiveTo: {
+        type: String,
+        // required: [true, "EffectiveTo is required"],
+      },
       approvalCertificate: {
         type: String,
         default: "",
